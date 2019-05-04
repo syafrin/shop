eb974c0578bb9052b5bf12cda4eb75fa45e61cc9
Syafrin <syafrinibrahim12@gmail.com>
2019-05-03 22:51:11 -0700

    update user

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 try {
-    $conn = new PDO("sqlsrv:server = tcp:webserversyafrin.database.windows.net,1433; Database = dbwebapp", "syafrin", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:webserversyafrin.database.windows.net,1433; Database = dbwebapp", "syafrin", "Android_123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -8,6 +8,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "syafrin@webserversyafrin", "pwd" => "{your_password_here}", "Database" => "dbwebapp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "syafrin@webserversyafrin", "pwd" => "Android_123", "Database" => "dbwebapp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:webserversyafrin.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
