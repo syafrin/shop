ce1295c1e8117d9fc548ce57092bd865af378476
Syafrin <syafrinibrahim12@gmail.com>
2019-05-03 22:50:07 -0700

    tes awal

---
+++ index.php
@@ -0,0 +1,13 @@
+try {
+    $conn = new PDO("sqlsrv:server = tcp:webserversyafrin.database.windows.net,1433; Database = dbwebapp", "syafrin", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "syafrin@webserversyafrin", "pwd" => "{your_password_here}", "Database" => "dbwebapp", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webserversyafrin.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
